25fbd7fccfb73f55c45fabf58b4c9c4aa928ab9c
ferrarofranco917-netizen <ferrarofranco917@gmail.com>
2026-03-31 20:14:11 +0200

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='kedrix-one-step3-fix';
-const FILES=['./','./index.html','./style.css','./manifest.json','./favicon.ico','./js/storage.js','./js/data.js','./js/utils.js','./js/wisemind.js','./js/templates.js','./js/app.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
