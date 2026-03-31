a678a44c416ff405921c4ca2ed3438ef06ee8917
ferrarofranco917-netizen <ferrarofranco917@gmail.com>
2026-03-31 21:50:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'kedrix-one-repo-complete-v1';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './favicon.ico',
+  './brand/kedrix-one-mark.svg',
+  './js/storage.js',
+  './js/data.js',
+  './js/utils.js',
+  './js/wisemind.js',
+  './js/templates.js',
+  './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
